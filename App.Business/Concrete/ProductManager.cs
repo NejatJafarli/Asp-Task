9c805abd0f6196321edd23f2cf651f0cbe6147d8
Nejat Jafarligmail.co <Mega.ceferli@gmail.com>
2022-10-02 00:13:21 +0400

    Add project files.

---
+++ App.Business/Concrete/ProductManager.cs
@@ -0,0 +1,46 @@
+﻿using App.Business.Abstract;
+using App.DataAccess.Abstract;
+using App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productDal.Delete(new Product { ProductId = productId });
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+            return _productDal.GetList(p => p.CategoryId == categoryId || categoryId==0);
+        }
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
---
+++ App.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using App.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index(int page = 1, int category = 0, string Orderby = "",string Order="asc")
+        {
+            int pageSize = 10;
+            var products = _productService.GetByCategory(category);
+
+            if (Orderby == "ProductName")
+            {
+                if (Order == "asc")
+                    products = products.OrderBy(p => p.ProductName).ToList();
+                else
+                    products = products.OrderByDescending(p => p.ProductName).ToList();
+            }
+            else if (Orderby == "UnitPrice")
+            {
+                if (Order == "asc")
+                    products = products.OrderBy(p => p.UnitPrice).ToList();
+                else
+                products = products.OrderByDescending(p => p.UnitPrice).ToList();
+            }
+
+            var model = new ProductListViewModel
+            {
+                Products = products.Skip((page - 1) * pageSize).Take(pageSize).ToList(),
+                PageCount = (int)Math.Ceiling(products.Count / (double)pageSize),
+                PageSize = pageSize,
+                CurrentCategory = category,
+                CurrentPage = page,
+                OrderBy=Orderby,
+                Order=Order
+            };
+            return View(model);
+        }
+    }
+}
